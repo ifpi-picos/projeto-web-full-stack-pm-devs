1e7db6216838ed7d5170125d2e96e5fcdbaa960d
Flávio Eduardo <xflax00@gmail.com>
2023-09-04 16:12:40 -0300

    feat: add express setup

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,2 +1,18 @@
-const test = "Node API...";
-console.log(test);
+import express from "express";
+import Router from "./Router";
+
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.use("/", Router);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`)
+});
