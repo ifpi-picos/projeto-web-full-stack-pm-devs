22f7164baaeb32b048cc095ff1f9eb673e5cce22
Ruan Lauro <ruanlauromonteiro@gmail.com>
2024-02-16 18:17:41 -0300

    New

--- API/prisma/migrations/20230906190415_add/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `profile_image` to the `Admin` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Admin" ADD COLUMN     "profile_image" TEXT NOT NULL;
--- API/prisma/migrations/20240216204844_first_mural/migration.sql
+++ API/prisma/migrations/20240216204844_first_mural/migration.sql
@@ -7,41 +7,31 @@ CREATE TABLE "User" (
     "password" TEXT NOT NULL,
     "profile_image" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Admin" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "phoneNumber" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Group" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "imgGroup" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
-    "adminId" TEXT NOT NULL,
+    "imgGroup" TEXT NOT NULL,
+    "groupCode" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Mural" (
-    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "category" TEXT NOT NULL,
-    "imgMural" TEXT NOT NULL
     "created_at" TIMESTAMP(3) NOT NULL,
+    "imgMural" TEXT NOT NULL,
     "groupId" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
 
     CONSTRAINT "Mural_pkey" PRIMARY KEY ("id")
 );
@@ -63,32 +53,41 @@ CREATE TABLE "Post" (
     "content" TEXT NOT NULL,
     "media" TEXT[],
     "created_at" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
-    "muralId" TEXT NOT NULL,
+    "muralId" INTEGER NOT NULL,
+    "memberId" TEXT NOT NULL,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Group_adminId_key" ON "Group"("adminId");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Group_groupCode_key" ON "Group"("groupCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Group_userId_key" ON "Group"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Member_userId_key" ON "Member"("userId");
 
 -- AddForeignKey
-ALTER TABLE "Group" ADD CONSTRAINT "Group_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "Admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Group" ADD CONSTRAINT "Group_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Mural" ADD CONSTRAINT "Mural_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Member" ADD CONSTRAINT "Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Member" ADD CONSTRAINT "Member_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Member" ADD CONSTRAINT "Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_muralId_fkey" FOREIGN KEY ("muralId") REFERENCES "Mural"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
